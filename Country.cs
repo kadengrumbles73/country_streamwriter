d64ec25bfe71921e7aea0b97f261bd835505ebc5
Kaden Grumbles <kaden.grumbles@gmail.com>
2026-07-01 09:45:11 -0700

    Created Country Class

---
+++ Country.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace PopulationDensity
+{
+    public class Country
+    {
+        public string Name { get; init;} //init makes the data immutable 
+        public string Region { get; init;}
+        public string SubRegion { get; init;}
+        public int Population { get; init;}
+        public double Area {get; init;}
+    public double CalculateDensity()
+    {
+        // cant divide by 0 so we would just return density as zero no negatives either
+        if (Area <= 0) 
+        {
+            return 0;
+        }
+        // if good data calculate density
+        else 
+        {
+            return Population / Area;
+        }
+    }
+
+    public string CleanString()
+        {
+            // Commas mess with csv format so replace
+            string CleanName = Name.Replace(",", ";");
+            double Density = CalculateDensity(); // get density
+            // format string thats ready for wCSV
+            return $"{CleanName}, {Region}, {SubRegion}, {Population}, {Area}, {Density}"; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PopulationDensity;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // STEP 1: Set up the outer safety net
+        try
+        {
+            // STEP 2: Open your local file gates and write CSV headers
+            // (Using StreamWriter)
+
+            // STEP 3: Now you are completely safe to initialize your HttpClient
+            // and open your StreamReader to pull the API data!
+        }
+        catch (Exception e)
+        {
+            // Catch any network or file drops here cleanly
+            Console.WriteLine($"An error occurred: {e.Message}");
+        }
+    }
+}
